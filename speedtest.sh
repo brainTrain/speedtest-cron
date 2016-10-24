3527a7f10e82cfb58bd0363c155ab40bcc46df1d
brainTrain <abductee@gmail.com>
2016-10-23 23:51:38 -0700

    update speedtest.sh
    * run parser
    * commit just log files that were generated
    * push to github repo with message "programmatic commit"

--- speedtest.sh
+++ speedtest.sh
@@ -8,3 +8,8 @@ date +%Y-%m-%dT%H:%M:%S%z >> $LOG_PATH
 $BIN/gdate +%s%N >> $LOG_PATH
 $BIN/speedtest --simple >> $LOG_PATH
 echo "-----" >> $LOG_PATH
+
+node parse.js
+
+git commit $BASEDIR/logs/*  -m "programmatic commit"
+git push origin master
