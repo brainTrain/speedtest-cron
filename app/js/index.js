c8cbec1da5cdd0c9ffa75f8b9d4408f9af9ee32e
brainTrain <abductee@gmail.com>
2016-11-13 12:27:04 -0800

    semi buggy but fully rendering data table
    * very simple/hard coded for now

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,5 @@
 import myAjax from './my-ajax';
 
-let dood = 'youz';
-console.log('dood', dood);
-
 myAjax(handleRequest, 'logs/speedtest.json', 'GET');
 
 function handleRequest(http) {
@@ -17,23 +14,34 @@ function handleRequest(http) {
     }
 }
 
+function tableHeaderParser(response) {
+    // only take first data object to get keys for header
+    const data = response[0].data;
+    return ['time', ...Object.keys(data)];
+}
+
+function tableBodyParser(response, order) {
+    return response.map((row) => {
+        return [row.date.iso, row.data.ping, row.data.download, row.data.upload];
+    });
+}
+
 function renderTable(response) {
-    console.log(JSON.parse(response)); 
+    const responseData = JSON.parse(response);
+    console.log(responseData); 
+
+    const tableHeaderData = tableHeaderParser(responseData)
+    const tableHeader = renderTableHeader(tableHeaderData);
+
+    const tableBodyData = tableBodyParser(responseData);
+    const tableBody = renderTableBody(tableBodyData);
+
     const table = document.getElementById('data-table');
-    const tableHeader = renderTableHeader();
-    const tableBody = renderTableBody();
     table.innerHTML = `${ tableHeader }${ tableBody }`;
 }
-
-function renderTableBody() {
-    const tableBodyContent = renderTableRow();
-    return `
-        <tbody>${ tableBodyContent }</tbody>
-    `;
-}
-
-function renderTableHeader() {
-    const tableHeaderCells = renderTableHeaderCell();
+// Table Header
+function renderTableHeader(content=[]) {
+    const tableHeaderCells = content.map((data) => { return renderTableHeaderCell(data); }).join('');
     return `
         <thead>
             <tr>${ tableHeaderCells }</tr>
@@ -42,11 +50,19 @@ function renderTableHeader() {
 }
 
 function renderTableHeaderCell(content='Header Cell') {
-    return `<th>${content}</th>`;
+    return `<th>${ content }</th>`;
 }
 
-function renderTableRow() {
-    const tableCells = renderTableCell();
+// Table Body
+function renderTableBody(content=[]) {
+    const tableBodyContent = content.map((data) => { return renderTableRow(data); }).join('');
+    return `
+        <tbody>${ tableBodyContent }</tbody>
+    `;
+}
+
+function renderTableRow(content=[]) {
+    const tableCells = content.map((data) => { return renderTableCell(data); }).join('');
     return `<tr>${ tableCells }</tr>`;
 }
 
